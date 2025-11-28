4cee6eede9b0ddc66309a01918d367750e6be9a2
Fadi nasr <nasr.fadi2511@gmail.com>
2025-11-28 11:01:37 +0200

    Feat: implement view-post

--- index.js
+++ index.js
@@ -6,19 +6,30 @@ const port=3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+let posts=[];  // array used here because we dont have a database.
+// console.log(posts)
+
 app.get("/",(req,res)=>{
     res.render("index.ejs")
 })
 
-app.get("/creat-post",(req,res)=>{
-    res.render("create-post.ejs")
+app.get("/create-post",(req,res)=>{
+    res.render("create-post.ejs");
 })
 
-app.post("/creat-post",(req,res)=>{
-    res.render("create-post.ejs")
+app.post("/",(req,res)=>{
+    posts.push({
+        title:req.body["title"],
+        post:req.body["post"]
+    })
+    console.log(posts)
+    res.redirect("/");
 })
 
+app.get("/view-post",(req,res)=>{
+    res.render("view-post.ejs",{ posts: posts });
+})
 
 app.listen(port,()=>{
-    console.log(`the server listening on port ${port}`)
+    console.log(`the server listening on port ${port}`);
 });
